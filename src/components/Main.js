86c818a07c0e641f72a783f4f675a469a1b7140e
Giuseppe Caponetto <giuseppe.caponetto@foodiestrip.com>
2017-07-29 19:05:03 +0200

    add working broadcast example

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,6 @@ import {
   View,
   TouchableOpacity
 } from 'react-native';
-import AudioSource from './AudioSource';
 import Sensors from './Sensors';
 
 const styles = StyleSheet.create({
@@ -32,10 +31,37 @@ const styles = StyleSheet.create({
 export default class Main extends Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      isMounted: false,
+      ws: null
+    };
   }
 
   componentDidMount() {
+    const host = 'wss://murmuring-dusk-99045.herokuapp.com';
+    // eslint-disable-next-line no-undef
+    const ws = new WebSocket(host);
+    ws.onopen = () => {
+      // connection opened
+      console.log(`Connection opened to ${host}`);
+    };
 
+    ws.onmessage = (e) => {
+      // a message was received
+      console.log(e.data);
+    };
+
+    ws.onerror = (e) => {
+      // an error occurred
+      console.log(e.message);
+    };
+
+    ws.onclose = (e) => {
+      // connection closed
+      console.log(e.code, e.reason);
+    };
+    this.state.isMounted = true;
+    this.state.ws = ws;
   }
   render() {
     return (
@@ -43,34 +69,35 @@ export default class Main extends Component {
         <Text style={styles.welcome}>
           Welcome to Audio Modulator!
         </Text>
-        <AudioSource
-          ref={(audio) => {
-            this.audio = audio;
-          }}
-          onReady={() => {
-            console.log('AudioSource is ready.');
-          }}
-        />
-        <Sensors
+        {/* <Sensors
           onUpdate={(data) => {
             // console.log('Sensor data updated: ', data);
             // console.log('Sensor data updated (orientation): ', JSON.stringify(data.orientation));
+            // eslint-disable-next-line max-len
             // console.log('Sensor data updated (accelerometer): ', JSON.stringify(data.accelerometer));
             // console.log('Sensor data updated (gyroscope): ', JSON.stringify(data.gyroscope));
             // console.log('Sensor data updated (light): ', JSON.stringify(data.light));
           }}
-        />
-        <TouchableOpacity onPress={() => {
-          this.audio.start();
-        }}
+        /> */}
+        <TouchableOpacity
+          onPress={() => {
+            if (this.state.isMounted && this.state.ws) {
+              console.log('Websocket testing midi message');
+              const message = { type: 'audiomodulator', payload: {} };
+              this.state.ws.send(JSON.stringify(message), () => {});
+            } else {
+              console.log('Websocket is not ready to send midi message.');
+            }
+          }}
         >
-          <Text>start</Text>
+          <Text style={{ fontSize: 24 }}>Send test</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          this.audio.stop();
-        }}
+        <TouchableOpacity
+          onPress={() => {
+            console.log('Button test');
+          }}
         >
-          <Text style={{ marginTop: 100 }}>stop</Text>
+          <Text style={{ fontSize: 24 }}>Button test</Text>
         </TouchableOpacity>
       </View>
     );
